949c16602a8238324950ecb73cea88724a4fabec
lzl <3012386836@qq.com>
2023-06-12 15:38:09 +0800

    lab02

---
+++ CS61A/labs/lab02/q5.py
@@ -0,0 +1,18 @@
+n = 7
+
+def f(x):
+    n = 8
+    return x + 1
+
+def g(x):
+    n = 9
+    def h():
+        return x + 1
+    return h
+
+def f(f, x):
+    return f(x + n)
+
+f = f(g, n)
+g = (lambda y: y())(f)
+print(g)
