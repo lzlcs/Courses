5134e3704cb9c3375fa0142c4d5a118c48738456
lzl <3012386836@qq.com>
2023-06-30 10:26:21 +0800

    lab12

---
+++ UCB_CS61A/labs/lab12/lab12.scm
@@ -0,0 +1,35 @@
+(define-macro
+ (if-macro condition if-true if-false)
+    `(if ,condition ,if-true ,if-false)
+)
+
+
+(define-macro (or-macro expr1 expr2)
+  `(let ((v1 ,expr1))
+     (if v1
+         v1
+         ,expr2)))
+
+(define (replicate x n) 
+  (if (= n 0)
+    () 
+    (cons x (replicate x (- n 1))))
+)
+
+
+(define-macro (repeat-n expr n) 
+    `(begin ,@(replicate expr (eval n))))
+
+
+(define
+ (list-of map-expr for var in lst if filter-expr)
+    `(map (lambda ,(cons var ()) ,map-expr) (filter (lambda ,(cons var ()) ,filter-expr) ,lst))) 
+
+(define-macro (list-of-macro map-expr
+                             for
+                             var
+                             in
+                             lst
+                             if
+                             filter-expr)
+  `(eval (list-of ',map-expr 'for ',var 'in ',lst 'if ',filter-expr)))
