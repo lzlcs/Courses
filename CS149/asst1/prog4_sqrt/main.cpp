fcdd28642990d42065311d601531e8fe520a3160
lzl <3012386836@qq.com>
2024-02-02 17:32:38 +0800

    CS149 assignment1 program4

--- CS149/asst1/prog4_sqrt/main.cpp
+++ CS149/asst1/prog4_sqrt/main.cpp
@@ -34,7 +34,10 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        if (i % 8 == 0) 
+            values[i] = 2.999;
+        else values[i] = 1.0;
     }
 
     // generate a gold version to check results
