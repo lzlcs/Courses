eb444d57843304afecad4f222739d126de238169
lzl <3012386836@qq.com>
2024-02-26 17:02:08 +0800

    back

--- Stanford_CS144/checkpoint0/minnow/apps/bidirectional_stream_copy.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "socket.hh"
-
-//! Copy socket input/output to stdin/stdout until finished
-void bidirectional_stream_copy( Socket& socket, std::string_view peer_name );
--- Stanford_CS144/checkpoint0/minnow/util/random.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <random>
-
-std::default_random_engine get_random_engine();
